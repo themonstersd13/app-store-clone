2e36c9bb0c9770a16ef435503388bc541a59d607
saurabh_doiphode <saurabhdoiphode1335@gmail.com>
2024-06-15 18:33:06 +0530

    part-1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -11,23 +11,23 @@ function App(){
   <TaglineHead 
   dark={taglineContent.first[i]} 
   content={taglineContent.second[i++]}/>
-  <NormalSc key={1} val={"sc1"} cont={scBar1} database={"1st\ scrollbar"}/>
+  <NormalSc key={1} val={"sc1"} cont={scBar1} database={"1stscrollbar"}/>
   <TaglineHead 
   dark={taglineContent.first[i]} 
   content={taglineContent.second[i++]}/>
-  <NormalSc key={2} val={"sc2"} cont={scBar2} database={"2nd\ scrollbar"}/>
+  <NormalSc key={2} val={"sc2"} cont={scBar2} database={"2ndscrollbar"}/>
   <TaglineHead 
   dark={taglineContent.first[i]} 
   content={taglineContent.second[i++]}/>
-  <NormalSc key={3} val={"sc4"} cont={scBar4} database={"4th\ scrollbar"}/>
+  <NormalSc key={3} val={"sc4"} cont={scBar4} database={"4thscrollbar"}/>
   <TaglineHead 
   dark={taglineContent.first[i]} 
   content={taglineContent.second[i++]}/>
-  <NormalSc key={4} val={"sc5"} cont={scBar5} database={"5th\ scrollbar"}/>
+  <NormalSc key={4} val={"sc5"} cont={scBar5} database={"5thscrollbar"}/>
   <TaglineHead 
   dark={taglineContent.first[i]} 
   content={taglineContent.second[i++]}/>
-  <NormalSc key={5} val={"sc6"} cont={scBar6} database={"6th\ scrollbar"}/>
+  <NormalSc key={5} val={"sc6"} cont={scBar6} database={"6thscrollbar"}/>
   </div>;
 };
 export default App;
